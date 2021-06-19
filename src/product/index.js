53c5b26372953472fd80c85a36ccbdd901c7ce51
Richard Melo <allucardster@gmail.com>
2021-06-19 11:38:14 -0500

    Fix code indentation

--- src/product/index.js
+++ src/product/index.js
@@ -1,11 +1,11 @@
 const render = require('../render');
 
 module.exports = (props) => {
-    const {product = null} = props;
+  const {product = null} = props;
 
-    if (null === product) {
-        render.throwError('Product Not Found', 404);
-    }
-
-    return render.template(`${__dirname}/product.html`, product);
+  if (null === product) {
+    render.throwError('Product Not Found', 404);
   }
+
+  return render.template(`${__dirname}/product.html`, product);
+}
